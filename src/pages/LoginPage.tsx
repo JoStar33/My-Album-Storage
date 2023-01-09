15ec84d73e9bcd482383ebc66843ed5adf734316
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-01-09 10:39:09 +0900

    쿠키삭제 시도

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import TextDialog from "../components/dialogs/commonDialogs/TextDialog";
 import LoginForm from "../components/forms/commonForms/LoginForm";
+import Cookies from 'universal-cookie';
 import { AppDispatch } from "../store/index";
 import { useDispatch } from "react-redux";
 import { resetUserState } from "../store/user";
@@ -11,11 +12,13 @@ import { Navigate } from "react-router-dom";
 import { getToken } from "../apis/tokens/token";
 
 const LoginPage: React.FC = () => {
+  const cookies = new Cookies();
   const isAuthorized = getToken();
   const dispatch = useDispatch<AppDispatch>();
   const [dialog, setDialog] = useState(false);
   const [dialogText, setDialogText] = useState(``);
   useEffect(() => {
+    cookies.remove('connect.sid');
     dispatch(resetUserState());
     dispatch(resetTopsterState());
     dispatch(resetAlbumState());
